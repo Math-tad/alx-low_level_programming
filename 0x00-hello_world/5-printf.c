5820e9b7ed89bd37f110498136d97076143324bf
Math-tad <mathi.tadesse@gmail.com>
2022-04-07 02:23:23 -0700

    Print a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print a given string using puts function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
