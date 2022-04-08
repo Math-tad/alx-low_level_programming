b7163654c046e9c13ce14110ba5ebcaa35832a23
Math-tad <mathi.tadesse@gmail.com>
2022-04-08 14:52:46 -0700

    Print lowercase alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting of the code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+
+	return (0);
+}
