8b2c9cde38314cc836845fd8aa9d31c853379664
Math-tad <mathi.tadesse@gmail.com>
2022-04-12 13:16:39 -0700

    Print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: character to be checked
- * Return: returns 1 if it is positive, returns 0 if it is 0 and returns -1 else
+ * Return: returns 1 if it is positiv, returns 0 if it is 0 and returns -1 else
  */
 
 int print_sign(int n)
