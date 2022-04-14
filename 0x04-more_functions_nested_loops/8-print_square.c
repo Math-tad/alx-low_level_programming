7b0f2509b01ac6d022b51e8c89d111bb0d7b2b88
Math-tad <mathi.tadesse@gmail.com>
2022-04-14 13:57:02 -0700

    Print square fusing a # character

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print a square
+ * @size: size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i = 1;
+	int j;
+
+	if (size > 0)
+	{
+		while (i <= size)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
