448b5fe8bab6ced482367b83405d40b54589fba2
Math-tad <mathi.tadesse@gmail.com>
2022-04-14 13:44:29 -0700

    Print 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print 0 to 9 exept 2 & 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i == '2' || i == '4')
+		{
+		}
+		else
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+
+}
