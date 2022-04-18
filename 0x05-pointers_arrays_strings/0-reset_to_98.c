c11566c0ae13343fa1ce1bfa053df4e864b0d503
Math-tad <mathi.tadesse@gmail.com>
2022-04-18 14:22:43 -0700

    Header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset pointer to point to 98
+ * @n: a pointer variable
+ * Return: returns void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
