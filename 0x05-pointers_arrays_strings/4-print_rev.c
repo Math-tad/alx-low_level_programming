aa595c943bb5e335026789a6e32c06422ae4a71b
Math-tad <mathi.tadesse@gmail.com>
2022-04-18 14:40:28 -0700

    Print reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print a string reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	s--;
+
+	while (len > 0)
+	{
+		putchar(*s);
+		len--;
+		s--;
+	}
+	putchar('\n');
+}
