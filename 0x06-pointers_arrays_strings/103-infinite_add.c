89c8a21c4e4e958fedc459d50142f0a45321c8ea
Math-tad <mathi.tadesse@gmail.com>
2022-04-22 14:47:16 -0700

    Add two numbers then store it on a defined sized variable

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * infinite_add - sum up two numbers
@@ -12,11 +14,24 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	r = n1 + n2;
+	long long int num1 = atoi(n1);
+	long long int num2 = atoi(n2);
+
+	int sum = num1 + num2;
+
+	int i;
+
 	if ((size_r + 1) >= sizeof(r))
 	{
+		while (i < sizeof(sum))
+		{
+			r[i] = sum % 10;
+			sum /= 10;
+			i++;
+		}
+		r = rev_str(r);
 		return (r);
 	}
 	else
-		return (0)
+		return (0);
 }
