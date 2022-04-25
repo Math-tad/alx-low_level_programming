0e0413612274478f5ca2794b36c74ec3aa46a3f2
Math-tad <mathi.tadesse@gmail.com>
2022-04-25 15:32:03 -0700

    Locate a substring (strstr)

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,35 +2,38 @@
 #define NULL 0
 
 /**
- * _strstr - the first mach of any bytes of accept
- * @s: string
- * @accept: prefix
- * Return: mach of any bytes of accept
+ * _strstr - locatefirst occurance of substring
+ * @haystack: string to search
+ * @needle: target substring to search
+ * Return: pointer to an index of first occurence
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int j = 0;
-	int co = 0;
+	int i = 0, j, x;
 
-	while (s[i] != '\0' && co != 1)
+	if (needle[0] == '\0')
+		return (haystack);
+
+	while (haystack[i] != '\0')
 	{
-		while (accept[j] != '\0')
+		if (haystack[i] == needle[0])
 		{
-			if (s[i] == accept[j] && accept[j] != ' ')
+			x = i, j = 0;
+			while (needle[j] != '\0')
 			{
-				co = 1;
-				break;
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
 			}
-
-			j++;
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 		i++;
 	}
-	if (co == 1)
-		return (&s[i]);
 
-	else
-		return (NULL);
+	return (NULL);
 }
