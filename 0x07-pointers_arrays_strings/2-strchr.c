6f305c184543b5bf1efeaf37491b9b7717a554f2
Math-tad <mathi.tadesse@gmail.com>
2022-04-25 14:51:26 -0700

    Find a char from string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,14 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	int j = 0;
 
 	while (s[i] != '\0' && s[i] != c)
-	{
 		i++;
-	}
+
 	if (s[i] == c)
-	{
 		return (&s[i]);
-	}
+
 	else
-	{
 		return (NULL);
-	}
+
 }
