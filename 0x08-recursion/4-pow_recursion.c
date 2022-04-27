ac74bc32a2ecab826b8a8b115cb2df89b8c9c4b8
Math-tad <mathi.tadesse@gmail.com>
2022-04-27 13:07:35 -0700

    x the power ofy

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power of a number
+ * @x: number
+ * @y: power
+ * Return: x raise to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
