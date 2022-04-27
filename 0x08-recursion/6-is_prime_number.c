d55b0cffdef85c81ac000185f86a303f1c1cb9f4
Math-tad <mathi.tadesse@gmail.com>
2022-04-27 13:14:56 -0700

    Prime number or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _prime - check number weather it is prime
+ * @num: number
+ * @r: checkng number
+ * Return: prime or not
+ */
+
+int _prime(int num, int r)
+{
+	if (r > 1 && r < num)
+	{
+		if (num % r != 0)
+			return (1);
+
+		return (_prime(num, r + 1));
+	}
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - check whether n is prime or not
+ * @n: number
+ * Return: 1 if it is prime and 0 if it is not
+ */
+
+int is_prime_number(int n)
+{
+	return (_prime(n, 2));
+}
