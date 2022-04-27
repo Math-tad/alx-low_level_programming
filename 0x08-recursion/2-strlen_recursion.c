bca7037a91eddef08b88c9e50d0aeec406a506c5
Math-tad <mathi.tadesse@gmail.com>
2022-04-27 13:01:54 -0700

    Length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts string length
+ * @s: string to be counted
+ * Return: number of char
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+
+	return (1 + _strlen_recursion(s));
+}
