761ce130547ae38a66bcab29b7cafcc0aa6e6a91
Math-tad <mathi.tadesse@gmail.com>
2022-04-29 06:57:49 -0700

    Program  to  print it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print name of a programe
+ * @argc: argument counter
+ * @argv: pointer to the array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
