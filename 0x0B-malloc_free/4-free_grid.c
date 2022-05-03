0beef7c6e8f77fd2aba031dea5cf0e1611398af4
Math-tad <mathi.tadesse@gmail.com>
2022-05-03 02:04:43 -0700

    Free a two dimentional array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a grid
+ * @grid: grid to be freed
+ * @height: height of the grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL)
+		free(grid);
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
