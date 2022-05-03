d1e044c77172b08805243beea78db4e62cfd9dd3
Math-tad <mathi.tadesse@gmail.com>
2022-05-03 01:35:01 -0700

    A similar function of a standard library strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to be duplicate
+ * Return: duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	int  len = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	p = malloc(sizeof(char) * len + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		p[i] = str[i];
+		i++;
+	}
+
+	return (p);
+}
