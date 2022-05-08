ee0e238cfe4372eb61858a1f988b7a7c5337feee
Math-tad <mathi.tadesse@gmail.com>
2022-05-08 02:18:47 -0700

    Allocate a memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - reserves place for amount of b
+ * @b: amount of memory to be reserved
+ * Return: pointer to the memory space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *sps;
+
+	sps = malloc(b);
+	if (sps == NULL)
+	{
+		exit(98);
+	}
+
+	return (sps);
+}
