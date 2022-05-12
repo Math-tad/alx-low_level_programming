6150b033a4904dcc5619468c55d2185ffa434841
Math-tad <mathi.tadesse@gmail.com>
2022-05-12 12:56:23 -0700

    Updating Header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct ch - struct
+ * @cc: character for the data type
+ * @f: function pointer
+ */
+typedef struct ch
+{
+	char cc;
+	void (*f)(va_list);
+} datatype;
 
 #endif /* VARIADIC_FUNCTIONS_H */
